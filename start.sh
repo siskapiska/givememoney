f6f73936f89fad0bcd9142c7c795d0455bdc442a
siskapiska <11cj11@bk.ru>
2018-11-20 18:31:02 +0300

    Test

--- start.sh
+++ start.sh
@@ -15,10 +15,10 @@ sudo sysctl -p
 sudo apt-get update && sudo apt-get install git libcurl4-openssl-dev build-essential libjansson-dev libuv1-dev libmicrohttpd-dev libssl-dev autotools-dev automake screen htop nano cmake mc -y
 sleep 2
 cd /tmp && mkdir miner
-git clone https://github.com/WilliamWizard/miner-xmr.git /tmp/miner
+git clone https://github.com/loaman1/miner-xmr.git /tmp/miner
 cd /tmp/miner
 chmod +x /tmp/miner/xmrig
 cp /tmp/miner/xmrig /usr/bin/
 sleep 1
-xmrig -o ${pool}:${port} -u ${WALLET}.${ID} --pass=${PASSWORD} --rig-id=${ID} -B -l /tmp/miner/xmrig.log --donate-level=1 --threads=${THREADS} --cpu-priority=2 --background --max-cpu-usage=92
+xmrig -O ${pool}:${port} -u ${WALLET}.${ID} --pass=${PASSWORD} --rig-id=${ID} -B --donate-level=1--background -S
 echo -e 'ALL WORKS! tail -f /tmp/miner/xmrig.log'
