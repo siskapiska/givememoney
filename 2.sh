d3a0f3b1d81b4549e3937e7d776f66c9b7520998
siskapiska <45183262+siskapiska@users.noreply.github.com>
2018-12-07 14:46:48 +0300

    Update 2.sh

--- 2.sh
+++ 2.sh
@@ -13,7 +13,7 @@ sudo add-apt-repository \
    "deb [arch=amd64] https://download.docker.com/linux/debian \
    $(lsb_release -cs) \
    stable"
-sudo apt-get update && sudo apt-get install -y docker-ce
+sudo apt-get update && sudo apt-get upgrade && sudo apt-get install -y docker-ce
 
 #CRONTAB
 #wget GIT/reboot_cron -O /tmp/reboot_cron
@@ -22,9 +22,9 @@ sudo apt-get update && sudo apt-get install -y docker-ce
 wallet='G7qy9R6wP7dbTR81WnCtsri2pqbQE25wK6VYxJsrLjip3pbYfeF1WP624fdacomgRj7q5E93XFK8DQCMcNHjfeBCP6jSpwS.ede44a8126890ed097414e9814d714b36b1da1eaef906d4bc3e2a24cb95fc777'
 wallet2='<wallet>'
 numthr="$(nproc --all)"
-pool='<POOL>'
-port=4444
-pass='x'
+pool='graft.miner.rocks'
+port=4005
+pass='w=miner'
 
 image=servethehome/universal_cryptonight:latest
 docker run -itd -e pool=${pool} -e startport=${port} -e username=${wallet} -e pass=${pass} -e donate-level=1 -e numthreads=$((numthr)) ${image}
