da83503dda3e1511de6475b91531c07b72a26a88
siskapiska <45183262+siskapiska@users.noreply.github.com>
2018-12-07 17:10:05 +0300

    Update start.sh

--- start.sh
+++ start.sh
@@ -12,7 +12,7 @@ sudo add-apt-repository \
    "deb [arch=amd64] https://download.docker.com/linux/debian \
    $(lsb_release -cs) \
    stable"
-sudo apt-get update && sudo apt-get upgrade && sudo apt-get install -y docker-ce
+sudo apt-get install -y docker-ce
 
 #CRONTAB
 #wget GIT/reboot_cron -O /tmp/reboot_cron
