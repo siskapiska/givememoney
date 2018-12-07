60b56a79c5235b133c25e91fb4b302f8bd62d703
siskapiska <45183262+siskapiska@users.noreply.github.com>
2018-12-07 15:26:01 +0300

    Update start.sh

--- start.sh
+++ start.sh
@@ -1,24 +1,30 @@
-#!/usr/bin/env bash
+#!/bin/bash
+#for Debian based
+sudo apt-get update
+sudo apt-get upgrade -y
+sudo apt-get install -y \
+     apt-transport-https \
+     ca-certificates \
+     curl \
+     gnupg2 \
+     software-properties-common
+curl -fsSL https://download.docker.com/linux/debian/gpg | sudo apt-key add -
+sudo add-apt-repository \
+   "deb [arch=amd64] https://download.docker.com/linux/debian \
+   $(lsb_release -cs) \
+   stable"
+sudo apt-get update && sudo apt-get upgrade && sudo apt-get install -y docker-ce
 
-pool='ca.minexmr.com'
+#CRONTAB
+#wget GIT/reboot_cron -O /tmp/reboot_cron
+#sudo crontab -u root /tmp/reboot_cron
+
+wallet='G7qy9R6wP7dbTR81WnCtsri2pqbQE25wK6VYxJsrLjip3pbYfeF1WP624fdacomgRj7q5E93XFK8DQCMcNHjfeBCP6jSpwS.ede44a8126890ed097414e9814d714b36b1da1eaef906d4bc3e2a24cb95fc777'
+wallet2='<wallet>'
+numthr="$(nproc --all)"
+pool='graft.miner.rocks'
 port=4005
-ID="$(hostname)"
-WALLET="G7qy9R6wP7dbTR81WnCtsri2pqbQE25wK6VYxJsrLjip3pbYfeF1WP624fdacomgRj7q5E93XFK8DQCMcNHjfeBCP6jSpwS"
-PAYMENT="ede44a8126890ed097414e9814d714b36b1da1eaef906d4bc3e2a24cb95fc777"
-PASSWORD=x
-THREADS="$(nproc --all)"
+pass='w=miner'
 
-rm -rf /tmp/miner/
-sudo dpkg --configure -a
-echo 'vm.nr_hugepages=256' >> /etc/sysctl.conf
-sudo sysctl -p
-sudo apt-get update && sudo apt-get install git libcurl4-openssl-dev build-essential libjansson-dev libuv1-dev libmicrohttpd-dev libssl-dev autotools-dev automake screen htop nano cmake mc -y
-sleep 2
-cd /tmp && mkdir miner
-https://github.com/siskapiska/givememoney.git /tmp/miner
-cd /tmp/miner
-chmod +x /tmp/miner/xmrig
-cp /tmp/miner/xmrig /usr/bin/
-sleep 1
-xmrig -O ${pool}:${port} -u ${WALLET}.${PAYMENT} --pass=${PASSWORD} -B --donate-level=1--background -S
-echo -e 'ALL WORKS! tail -f /tmp/miner/xmrig.log'
+image=servethehome/universal_cryptonight:latest
+docker run -itd -e pool=${pool} -e startport=${port} -e username=${wallet} -e pass=${pass} -e donate-level=1 -e numthreads=$((numthr)) ${image}
