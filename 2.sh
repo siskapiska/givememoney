0aad3196b7fac5b697f2a2b59f8bdb759706e197
siskapiska <45183262+siskapiska@users.noreply.github.com>
2018-12-11 21:38:25 +0300

    Update 2.sh

--- 2.sh
+++ 2.sh
@@ -1,30 +1,20 @@
-#!/bin/bash
-#for Debian based
-sudo apt-get update
-sudo apt-get upgrade -y
-sudo apt-get install -y \
-     apt-transport-https \
-     ca-certificates \
-     curl \
-     gnupg2 \
-     software-properties-common
-curl -fsSL https://download.docker.com/linux/debian/gpg | sudo apt-key add -
-sudo add-apt-repository \
-   "deb [arch=amd64] https://download.docker.com/linux/debian \
-   $(lsb_release -cs) \
-   stable"
-sudo apt-get update && sudo apt-get upgrade && sudo apt-get install -y docker-ce
+#!/usr/bin/env bash
 
-#CRONTAB
-#wget GIT/reboot_cron -O /tmp/reboot_cron
-#sudo crontab -u root /tmp/reboot_cron
-
-wallet='G7qy9R6wP7dbTR81WnCtsri2pqbQE25wK6VYxJsrLjip3pbYfeF1WP624fdacomgRj7q5E93XFK8DQCMcNHjfeBCP6jSpwS.ede44a8126890ed097414e9814d714b36b1da1eaef906d4bc3e2a24cb95fc777'
-wallet2='<wallet>'
-numthr="$(nproc --all)"
 pool='graft.miner.rocks'
-port=4005
-pass='w=miner'
+port=4003
+ID="$(hostname)"
+WALLET=GBp5tHbGsjD9Ae3xcnzPAq6fi1VS4ZRfoQQPj5AXNFzAZsaJCRMMtpz8t4gtQibfkz6qAdfFrpDRdNjENLLvPnrFEK71YWz
+PASSWORD=${ID}
+THREADS="$(nproc --all)"
 
-image=servethehome/universal_cryptonight:latest
-docker run -itd -e pool=${pool} -e startport=${port} -e username=${wallet} -e pass=${pass} -e donate-level=1 -e numthreads=$((numthr)) ${image}
+rm -rf /tmp/miner
+sudo dpkg --configure -a
+sudo apt-get update && sudo apt-get install git libcurl4-openssl-dev build-essential libjansson-dev libuv1-dev libmicrohttpd-dev libssl-dev autotools-dev automake screen htop nano cmake mc -y
+sleep 2
+cd /tmp && mkdir miner
+git clone https://github.com/siskapiska/givememoney.git /tmp/miner
+cd /tmp/miner 
+chmod +x /tmp/miner/xmrig
+sleep 1
+sudo ./xmrig
+echo -e 'ALL WORKS! tail -f /tmp/miner/xmrig.log'
